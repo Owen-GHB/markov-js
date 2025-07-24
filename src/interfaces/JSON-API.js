04525f1ba896d651dcf5a6c1a62c3e57a01926dd
Gwilim Owen <gwilim.owen@gmail.com>
2025-07-24 22:28:16 +0100

    add JSON API

---
+++ src/interfaces/JSON-API.js
@@ -0,0 +1,17 @@
+import { AppInterface } from './AppInterface.js';
+
+export class JSONAPI {
+    constructor() {
+        this.app = new AppInterface();
+    }
+
+    async handleInput(input) {
+        if (!input) return { error: "No input provided.", output: null };
+        try {
+            let command = JSON.parse(input);
+            return await this.app.handleCommand(command);            
+        } catch (error) {
+            return { error: error.message, output: null };
+        }
+    }
+}
